e326cf7e9312b43b08cdd2c605f5b9fda9d2171b
Dean <deangalvin3@gmail.com>
2016-08-09 11:03:08 -0400

    added basic validations

---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    username 'test_username'
+    password 'test_pass'
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it 'has a valid factory' do
+    expect(user).to be_valid
+  end
+
+  describe 'validations' do
+    it 'does not allow an empty username' do
+      expect(FactoryGirl.build(:user, username: nil)).to_not be_valid
+    end
+  end
+end
